432e2326d7a1523e2f1d765ae9b379fb712eda74
Alex Bové Torres <bove.abt@gmail.com>
2026-07-08 22:15:55 +0200

    cambio de puerto

--- server.ts
+++ server.ts
@@ -21,7 +21,7 @@ const db = getFirestore(firebaseApp, firebaseConfig.firestoreDatabaseId);
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = parseInt(process.env.PORT || "3000");
 
   app.use(express.json());
 
